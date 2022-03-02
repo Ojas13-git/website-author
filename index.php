f6ad02fd5f887302f3c1bb11ccdf4708d9973290
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2022-03-02 18:57:15 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
-ECHO "ojas";
+// ECHO "ojas";
 //get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "gprateek0402@gmail.com";
+$to = "ojas.palorkar@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: webevolve01@gmail.com" . "\r\n" .
-"CC: ojas.palorkar@gmail.com";
+$headers = "From: gprateek0402@gmail.com" . "\r\n" .
+"CC: ayushpatil010@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
+    header("Location:thankyou.html");
 }
 // redirect
-header("Location:thankyou.html");
+
 ?>
